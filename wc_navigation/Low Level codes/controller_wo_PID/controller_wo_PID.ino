3b33431c2650f9179898641a6010efde8ce95deb
Aakash872 <aakashtiwari07.12.2003@gmail.com>
2023-12-28 13:04:38 +0530

    edit

--- wc_navigation/Low Level codes/controller_wo_PID/controller_wo_PID.ino
+++ wc_navigation/Low Level codes/controller_wo_PID/controller_wo_PID.ino
@@ -2,10 +2,20 @@
 #include <ros.h>
 #include <geometry_msgs/Twist.h>
 #include <std_msgs/String.h>
+//imu -start
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+#include <sensor_msgs/Imu.h>
+//imu -end
 #define PWM_MIN 20
 #define PWMRANGE 255
 #define L 0.5
 #define R 0.1615
+//imu -start
+#define BNO055_SAMPLERATE_DELAY_MS (100)
+//imu -end
 bool _connected = false;
 bool rosConnected();
 void onTwist(const geometry_msgs::Twist &msg);
@@ -23,8 +33,12 @@ ros::Publisher chatter("debug", &str_msg);
 int lp=0,rp=0;
 
 //Consider maximum velocity=0.1 at which corresponding pwm value is noted. This is used in the low level controller
-
-
+//imu -start
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+//ros::NodeHandle nh;
+sensor_msgs::Imu imu_msg;
+ros::Publisher imu_pub("/imu_data", &imu_msg);
+//imu -end
 
 void setup()
 {
@@ -32,6 +46,13 @@ void setup()
   node.initNode();
   node.subscribe(sub);
   node.advertise(chatter);
+  //imu -start
+  Wire.begin();
+  bno.begin();
+  //nh.initNode();
+  node.advertise(imu_pub);
+  delay(1000);
+  //imu -end
 }
 
 void onTwist(const geometry_msgs::Twist &msg)
@@ -47,7 +68,7 @@ void onTwist(const geometry_msgs::Twist &msg)
   if(l==0&&r==0)
   {
     lPwm=0;
-    rPwm=0;
+   rPwm=0;
   }
   if( l > 0){
     lPwm = 10 + (10.84599 * l);
@@ -62,7 +83,7 @@ void onTwist(const geometry_msgs::Twist &msg)
   else{
     rPwm = -10 + (10.84599 * r);
     }
-    
+
   char temp[30];
   String t="lpWM : " + String(lPwm) + " " + "rpWM : " + String(rPwm)+"l: "+String(l)+" r: "+String(r);
   t.toCharArray(temp,40);
@@ -78,7 +99,37 @@ void loop()
   {
      Serial.println("Stopped");
   }
+  //node.spinOnce();
+  //imu -start
+  sensors_event_t event;
+  bno.getEvent(&event);
+
+  imu::Quaternion quat = bno.getQuat();
+  
+  imu_msg.header.stamp = node.now();
+  imu_msg.header.frame_id = "imu_link";
+  
+  imu_msg.orientation.x = quat.x();
+  imu_msg.orientation.y = quat.y();
+  imu_msg.orientation.z = quat.z();
+  imu_msg.orientation.w = quat.w();
+        // Get angular velocity
+  imu::Vector<3> ang_velocity = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+  imu_msg.angular_velocity.x = ang_velocity.x();
+  imu_msg.angular_velocity.y = ang_velocity.y();
+  imu_msg.angular_velocity.z = ang_velocity.z();
+  
+  // Get linear acceleration
+  imu::Vector<3> lin_acceleration = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  imu_msg.linear_acceleration.x = lin_acceleration.x();
+  imu_msg.linear_acceleration.y = lin_acceleration.y();
+  imu_msg.linear_acceleration.z = lin_acceleration.z();
+  
+  imu_pub.publish(&imu_msg);
+
   node.spinOnce();
+  delay(BNO055_SAMPLERATE_DELAY_MS);
+  //imu -end
 }
 
 bool rosConnected()
